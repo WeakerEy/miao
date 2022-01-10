b80119fe800233c062ecc1ca6227deb52169638e
WeakerEy <280676418@qq.com>
2022-01-10 16:34:11 +0800

    new

--- lodash/weaker-lodash.js
+++ lodash/weaker-lodash.js
@@ -1,11 +1,17 @@
 var weakerey = {
   chunk: function (array, size) {
     var arr = []
-    arr.length = size
-    var start = 0
+    var result = []
     for (var i = 0; i < array.length; i++) {
-      arr[start] = array[i]
-      start++
+      arr.push(array[i])
+      if (arr.length == size) {
+        result.push(arr)
+        arr = []
+      }
     }
+    if (arr.length != 0) {
+      result.push(arr)
+    }
+    return result
   }
 }
